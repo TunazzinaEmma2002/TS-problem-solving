c283b19f0fa1006fcc272f6e29df00e3dad1c38c
TunazzinaEmma2002 <taposerabeya2048@gmail.com>
2026-08-20 23:48:32 +0600

    first commit

---
+++ dist/formatUserProfile.js
@@ -0,0 +1,16 @@
+"use strict";
+// type User = {
+//     name:string
+//     age: number
+//     city: string
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+const formatUserProfile = (user) => {
+    return `${user.name} is ${user.age} years old and lives in ${user.city}.`;
+};
+console.log(formatUserProfile({
+    name: "Fahim",
+    age: 22,
+    city: "Dhaka"
+}));
+//# sourceMappingURL=formatUserProfile.js.map
---
+++ src/calculateCartTotal.ts
@@ -0,0 +1,28 @@
+type Product = {
+    name:string
+    price: number
+}
+
+const calculateCartTotal = (products:Product[]):number  => {
+
+    const total:number = products.reduce((sum, item) => sum + item.price, 0 )
+    return total
+
+}
+const products = [
+    { name: "Keyboard", price: 1500 },
+    { name: "Mouse", price: 800 },
+    { name: "USB Cable", price: 300 }
+];
+
+const products2 = [
+    { name: "Book", price: 500 },
+    { name: "Pen", price: 50 },
+    { name: "Bag", price: 1200 }
+];
+ 
+
+
+console.log(calculateCartTotal(products));
+console.log(calculateCartTotal(products2));
+
